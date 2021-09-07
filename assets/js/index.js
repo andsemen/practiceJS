653c106dbad3a2ae54b073f24095ea76e20d5cff
Andrey Semenchenko <andrew.semenchenko.94@gmail.com>
2021-09-07 21:25:42 +0300

    fixed daofAplication

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,24 +56,34 @@ const student = {
 
 const arr = new Array(25).fill(null).map((_, i) => i + 7);
 
+//3.2
 const evenArrIndex = arr.filter((_, i) => i % 2 === 0);
 
+//3.3
 for (const elem of arr) {
   elem % 2 === 0 ? console.log(elem) : null;
 }
 
+//3.4
 arr[12] = 0;
 arr[19] = 0;
 arr[6] = 0;
+
+const zeroElemIndex = arr.map((elem, index) => 
+ elem === 0 ? index : undefined).filter(Boolean);
+
+//3.5
+const zeroElem = arr.filter((elem) => elem === 0);
+console.log(zeroElem.length);
 
 //4
 
 class Book {
   constructor(autor, bookTitle, wasWritten, publisher) {
-    (this.autor = autor),
-      (this.bookTitle = bookTitle),
-      (this.wasWritten = wasWritten),
-      (this.publisher = publisher);
+    this.autor = autor,
+    this.bookTitle = bookTitle,
+    this.wasWritten = wasWritten,
+    this.publisher = publisher;
   }
 }
 
@@ -81,23 +91,21 @@ class EBook extends Book {
   constructor(autor, bookTitle, wasWritten, publisher, typeOfBook, eNumber) {
     super(autor, bookTitle, wasWritten, publisher);
 
-    this.typeOfBook = typeOfBook, 
+    this.typeOfBook = typeOfBook; 
     this.eNumber = eNumber;
   }
 }
 
-
 //5
 
 function fizzBuzz(numb) {
-  
-  for(let i = 1; i <= numb; i++) {
-    if (i % 3 ===0 && i % 5 === 0) {
-      console.log('FizzBuzz');
-    } else if (i % 3 ===0) {
-      console.log('Fizz');
-    } else if(i % 5 ===0) {
-      console.log('Buzz');
+  for (let i = 1; i <= numb; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
     } else {
       console.log(i);
     }
@@ -106,10 +114,87 @@ function fizzBuzz(numb) {
 
 //6
 
-class Students extends student {
-  constructor(name,lastName,isMale,contacts,spesialetyInfo,dateOfApplication) {
-    super(name,lastName,isMale,contacts,spesialetyInfo)
+class Student {
+  constructor(name, lastName, isMale, contacts, dateOfApplication = new Date()) {
+    this.name = name;
+    this.lastName = lastName;
+    this.isMale = isMale;
+    this.contacts = contacts;
+    this.dateOfApplication = dateOfApplication;
+  }
 
-    this._spesialetyInfo = spesialetyInfo
+  set name(text) {
+    if (typeof text !== "string") {
+      throw new TypeError("Invalid type");
+    }
+    if (text.trim() === "") {
+      throw new Error("Name must not to be empty");
+    }
+
+    this._name = text;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set lastName(text) {
+    if (typeof text !== "string") {
+      throw new TypeError("Invalid type");
+    }
+    if (text.trim() === "") {
+      throw new Error("Last Name must not to be empty");
+    }
+
+    this._lastname = text;
+  }
+
+  get lastName() {
+    return this._lastname;
+  }
+
+  set isMale(text) {
+    if (typeof text !== "boolean") {
+      throw new TypeError("Invalid Type");
+    }
+
+    this._isMale = text;
+  }
+
+  get isMale() {
+    return this._isMale;
+  }
+
+  set contacts(numb) {
+    if (isNaN(numb) || typeof numb !== "number") {
+      throw new TypeError("Invalid Type");
+    }
+
+    this._contacts = numb;
+  }
+
+  get contacts() {
+    return this._contacts;
+  }
+
+  set dateOfApplication(str) {
+    const date = new Date(str);
+    const today = new Date()
+if (today.getFullYear()-date.getFullYear()  > 5) {
+  throw new Error('Too old bro')
+}
+
+  this._dateOfApplication = date;
+
+  }
+
+  get dateOfApplication() {
+    return this._dateOfApplication
+  }
+
+  get course() {
+    const thisYear = new Date();
+
+    return thisYear.getFullYear() - this._dateOfApplication.getFullYear();
   }
 }
